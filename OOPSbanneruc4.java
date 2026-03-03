4801bb95d80d041bec9ca535ecb171eed77d90e8
surya0861 <vn5596@srmist.edu.in>
2026-03-03 15:16:46 +0530

    code added

---
+++ OOPSbanneruc4.java
@@ -0,0 +1,48 @@
+public class OOPSbanneruc4 {
+
+    public static void main(String[] args) {
+
+        // 7 lines for OOPS banner
+        System.out.println(String.join(" ",
+                "*******",
+                "*******",
+                "*******",
+                "*******"));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*      "));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*******",
+                " ***** "));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "      *"));
+
+        System.out.println(String.join(" ",
+                "*     *",
+                "*     *",
+                "*      ",
+                "*     *"));
+
+        System.out.println(String.join(" ",
+                "*******",
+                "*******",
+                "*      ",
+                " ***** "));
+    }
+}
